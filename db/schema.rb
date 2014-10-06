44f9acfbc2ba1d192843ef137b5b29cb381e0d83
Max Wofford <max.speed.wofford@gmail.com>
2014-10-05 19:36:15 -0600

    add cards

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141006012745) do
+
+  create_table "cards", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
